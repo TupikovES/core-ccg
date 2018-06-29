04f35ebd66a5fdd9223a116682a5319da0c28e0e
Tupikov_ES <TupikovES@samnipineft.ru>
2018-06-29 16:00:44 +0400

    Initial commit

---
+++ src/test/java/com/ccg/coreccg/CoreCcgApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ccg.coreccg;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CoreCcgApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
