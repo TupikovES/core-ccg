a16a242a68b078d9d6604c36aa369a50ac71b141
Tupikov_ES <TupikovES@samnipineft.ru>
2018-07-04 17:10:30 +0400

    new project files

---
+++ src/main/java/com/ccg/coreccg/view/controller/MainController.java
@@ -0,0 +1,16 @@
+package com.ccg.coreccg.view.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+
+    @RequestMapping(value = {"", "/"}, method = RequestMethod.GET)
+    public String getMainPage(Model modal) {
+        return "main";
+    }
+
+}
